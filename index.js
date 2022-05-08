9d3273db5126a2928bbf92de48e87ea4f50530bb
nosianuri <nurinosia@gmail.com>
2022-05-08 20:41:49 +0600

    mongodb data access

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sai9t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db('warehouse').collection('items');
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+        app.get('/items/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const item = await itemsCollection.findOne(query);
+            res.send(item);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello from my warehouse server!!')
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
