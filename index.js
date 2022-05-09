ffd6b2262b321b30b8936999cf11d4811465a3f4
nosianuri <nurinosia@gmail.com>
2022-05-09 22:20:17 +0600

    working in heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -10,7 +11,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
 
+    
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sai9t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -19,8 +36,20 @@ async function run() {
     try {
         await client.connect();
         const itemsCollection = client.db('warehouse').collection('items');
-        const manageCollection = client.db('warehouse').collection('manage')
+        const manageCollection = client.db('warehouse').collection('manage');
 
+        // AUTH
+        app.post('/login', async (req, res) => {
+
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+
+        // Items API
         app.get('/items', async (req, res) => {
             const query = {};
             const cursor = itemsCollection.find(query);
@@ -28,45 +57,52 @@ async function run() {
             res.send(items);
         });
 
-        app.get('/items/:id', async(req, res) =>{
+        app.get('/items/:id', async (req, res) => {
             const id = req.params.id;
-            const query={_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const item = await itemsCollection.findOne(query);
             res.send(item);
         });
 
         //POST
-        app.post('/items', async(req, res) =>{
+        app.post('/items', async (req, res) => {
             const newItem = req.body;
             const result = await itemsCollection.insertOne(newItem);
             res.send(result);
         });
 
-        
+
 
         //DELETE
-        app.delete('/items/:id', async(req, res) =>{
+        app.delete('/items/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await itemsCollection.deleteOne(query);
             res.send(result);
         });
 
         // Item Collection API
 
-        app.get('/myitem', async(req, res) =>{
-            const query = {};
-            const cursor = itemsCollection.find(query);
-            const item = await cursor.toArray();
-            res.send(item);
+        app.get('/manage', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = {};
+                const cursor = itemsCollection.find(query);
+                const item = await cursor.toArray();
+                res.send(item);
+            }
+            else{
+                res.status(403).send({message: 'forbidden access'})
+            }
         });
 
 
         // manage Collection API
-        app.post('/manage', async(req, res) =>{
+        app.post('/manage', async (req, res) => {
             const manage = req.body;
             const result = await manageCollection.insertOne(manage);
-            res.send (result);
+            res.send(result);
         })
     }
     finally {
